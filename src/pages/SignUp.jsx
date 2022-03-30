f052e813554af351949a1bfb3e6be39ca94500bb
Philipp Hofmann <98397811+HofmannPhilipp@users.noreply.github.com>
2022-03-30 22:20:18 +0200

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {ReactComponent as PersonIcon} from "../assets/svg/personIcon.svg"
+import {ReactComponent as LockIcon} from "../assets/svg/lockIcon.svg"
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <>
+    <header className=''>
+      <div className='mt-6 mx-5'>
+        <h1 className='font-bold text-2xl'>Welcome Back!</h1>
+        <form className='mt-8 space-y-4'>
+          <div className='flex bg-white py-1 rounded-full'>
+            <PersonIcon className='mx-3'/>
+            <input type="text" placeholder='Email' className='outline-0 w-4/5'/>
+            </div>
+            <div className='flex bg-white py-1 rounded-full'><LockIcon className='mx-3'/><input type="text" placeholder='Password' className='outline-0 w-4/5'/></div></form>
+      </div>
+      
+    </header>
+    </>
   )
 }
 
